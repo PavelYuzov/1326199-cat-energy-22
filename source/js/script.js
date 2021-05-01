bfce386c0162e1268f80aa0d777af2f75f3cb94e
PavelYuzov <60073022+PavelYuzov@users.noreply.github.com>
2021-05-02 01:19:37 +0300

    6 раздел form.1.2

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,15 @@
-const main = document.querySelector(".main-nav");
-const main_header = document.querySelectorAll(".page-header");
-const button_main = document.querySelector(".page-header__button");
-const button_burger = document.querySelector(".page-header__button--burger");
-const button_dagger = document.querySelector(".page-header__button--dagger");
-const button_nojs = document.querySelector(".page-header__button--nojs");
-const main_close = document.querySelector(".main-nav--close");
-const main_nojs = document.querySelector(".main-nav--nojs");
+const mainNav = document.querySelector(".main-nav");
+const mainHeader = document.querySelectorAll(".page-header");
+const menuButton = document.querySelector(".page-header__button");
 
-main.classList.remove("main-nav--nojs");
-button_main.classList.remove("page-header__button--nojs");
+mainNav.classList.remove("main-nav--nojs");
+menuButton.classList.remove("page-header__button--nojs");
 
-button_main.addEventListener("click", function(evt) {
+menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
 
-  main.classList.toggle("main-nav--close");
+  mainNav.classList.toggle("main-nav--close");
 
-  button_main.classList.toggle("page-header__button--burger");
-  button_main.classList.toggle("page-header__button--dagger");
+  menuButton.classList.toggle("page-header__button--open");
+  menuButton.classList.toggle("page-header__button--close");
 });
